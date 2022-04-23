fb618824611cb68ed85052a33f2db2c41ee543be
NNLong1208 <ngoclong1282001@gmail.com>
2022-04-23 16:29:48 +0700

    config link

--- chat_bot.py
+++ chat_bot.py
@@ -1,6 +1,7 @@
 import asyncio
 import time
-
+import json
+import random
 from rasa.core.agent import Agent
 
 
@@ -8,6 +9,8 @@ class Model:
 
     def __init__(self, model_path=r"./models/20220418-210445-burning-borzoi.tar.gz") -> None:
         self.agent = Agent.load(model_path)
+        f = open('./config.json', encoding='utf-8')
+        self.config = json.load(f)
         print("NLU model loaded")
 
     def predict(self, message: str) -> str:
@@ -19,16 +22,29 @@ class Model:
         message = message.strip()
         results = asyncio.run(self.agent.handle_text(message))
         output = self.predict(message)
-        print(output)
         for result in results:
             result['text'] = self.format_output(result['text'], output)
         return results
 
-    @staticmethod
-    def format_output(text, output):
+    def format_output(self, text, output):
+        keys = list(self.config)
+        keys.remove('domain')
         if output['intent']['name'] == 'ask_product':
             for entity in output['entities']:
                 text = text.replace(entity['entity'], entity['value'])
+            # for key in keys:
+            #     text = text.replace(key, self.config['domain'] + self.config[key])
+            # if "<brand/product>" in text:
+            #     text = "Xin lỗi bạn, mình không tìm thấy sản phẩm. Mình giới thiệu bạn sản phẩm này nè{}".format( self.config['domain'] + self.config[random.choice(keys)])
+            try:
+                start = text.index("<")
+                end = text.index(">")
+                key = text[start:end+1]
+                text = text.replace(key, self.config['domain'] + self.config[key])
+            except Exception:
+                text = "Xin lỗi bạn, mình không tìm thấy sản phẩm. Mình giới thiệu bạn sản phẩm này nè {}".format(self.config['domain'] + self.config[random.choice(keys)])
+            text = text.replace("<", "")
+            text = text.replace(">", "")
         return text
 
 
